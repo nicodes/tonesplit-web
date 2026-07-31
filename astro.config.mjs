14a9b30995f256913e652863e9af1cee67305f14
nicodes <nicozessoules@gmail.com>
2026-07-30 19:38:58 -0600

    init astro app

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import qwikdev from '@qwikdev/astro';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [qwikdev()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
